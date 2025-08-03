0dbf575e315ba401b6fea9d872753779acbf6ce5
m-ghanem-dev <mohamedgh4444@gmail.com>
2025-08-03 22:32:34 +0200

    run sql scripts on startup once

--- analytics/app.py
+++ analytics/app.py
@@ -11,6 +11,29 @@ from config import app, db
 
 
 port_number = int(os.environ.get("APP_PORT", 5153))
+sql_files = [ "db/1_create_tables.sql", "db/2_seed_users.sql","db/3_seed_tokens.sql"]
+
+def run_sql_files_once(file_paths, marker_path=".init_sql_applied"):
+    if os.path.exists(marker_path):
+        app.logger.info("Init SQL already applied. Skipping.")
+        return
+
+    with app.app_context():
+        try:
+            for path in file_paths:
+                with open(path, "r") as f:
+                    sql = f.read()
+                db.session.execute(text(sql))
+                app.logger.info(f"Executed SQL file: {path}")
+
+            db.session.commit()
+
+            # Create the marker file
+            with open(marker_path, "w") as marker:
+                marker.write("applied\n")
+        except Exception as e:
+            db.session.rollback()
+            app.logger.error(f"Failed during SQL execution: {e}")
 
 
 @app.route("/health_check")
@@ -82,4 +105,5 @@ job = scheduler.add_job(get_daily_visits, 'interval', seconds=30)
 scheduler.start()
 
 if __name__ == "__main__":
+    run_sql_files_once(sql_files)
     app.run(host="0.0.0.0", port=port_number)
